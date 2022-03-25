4215e54cf3bbba2cc04f6076e3e5a5520002e3e7
jayson morgado <jaysonm@udel.edu>
2022-03-25 01:15:30 -0400

    added movement and collisions

--- Adventure game/level.py
+++ Adventure game/level.py
@@ -1,25 +1,34 @@
-import pygame
+import pygame 
+from settings import *
 from tile import Tile
 from player import Player
-from settings import *
+from debugging import debug
 
 class Level:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
-        ##sprites
-        self.visible_sprites = pygame.sprite.Group()
-        self.obstacle_sprites = pygame.sprite.Group()
-        self.create_map()
+	def __init__(self):
 
-    def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for column_index, column in enumerate(row):
-                x = column_index * TILESIZE
-                y = row_index * TILESIZE
-                if column == 'x':
-                    Tile((x,y), [self.visible_sprites])
-                if column == 'p':
-                    Player((x,y), [self.visible_sprites])
-    def run(self):
-        ## update and redraw
-        self.visible_sprites.draw(self.display_surface)
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
+
+		# sprite group setup
+		self.visible_sprites = pygame.sprite.Group()
+		self.obstacle_sprites = pygame.sprite.Group()
+
+		# sprite setup
+		self.create_map()
+
+	def create_map(self):
+		for row_index,row in enumerate(WORLD_MAP):
+			for col_index, col in enumerate(row):
+				x = col_index * TILESIZE
+				y = row_index * TILESIZE
+				if col == 'x':
+					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+				if col == 'p':
+					self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+
+	def run(self):
+		# update and draw the game
+		self.visible_sprites.draw(self.display_surface)
+		self.visible_sprites.update()
+		debug(self.player.direction)
